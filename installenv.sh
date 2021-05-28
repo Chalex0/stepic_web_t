fc8d60abfc3041b4c6ca33f0e4318da2af37036c
Chalex0 <chalex0@gmail.com>
2021-05-28 21:32:03 +0300

    28.05.2021

--- installenv.sh
+++ installenv.sh
@@ -1,16 +1,15 @@
-sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update && sudo apt install python3.6
+sudo apt update
+sudo apt install python3.5 -y
+sudo apt install python3.5-dev -y
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo python3 -m pip install gunicorn
+sudo python3 -m pip install django==2.0
+sudo python3 -m pip install mysqlclient
 
-sudo rm /usr/bin/python3 && sudo ln -s /usr/bin/python3.6 /usr/bin/python3
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE stepic_web;"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web.* TO 'box'@'localhost' WITH GRANT OPTION;"
 
-curl "https://bootstrap.pypa.io/ez_setup.py" -o "ez_setup.py" && curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-
-sudo python3 ez_setup.py && sudo python3 get-pip.py
-
-sudo -H /usr/local/bin/pip3 install --upgrade django==3.1
-
-pip install gunicorn
-
-cd /home/box/web/ask
-
-gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=15 --log-level=debug ask.wsgi:application
-
+python3 manage.py makemigrations qa
+python3 manage.py migrate
