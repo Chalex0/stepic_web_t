47a443608e3fb59ef5abba6572bf20f6755fb7e6
Chalex0 <chalex0@gmail.com>
2021-05-28 17:02:37 +0300

    28.05.2021

--- init.sh
+++ init.sh
@@ -7,9 +7,9 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo rm /etc/gunicorn.d/hello.py
-#sudo rm /etc/gunicorn.d/django.py
+sudo rm /etc/gunicorn.d/django.py
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-#sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
+sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
 
 sudo /etc/init.d/gunicorn restart
